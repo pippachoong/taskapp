9836947178860f4b970f778b3df72127db36e3c4
Philippa Choong <pippachoong@gmail.com>
2023-01-05 21:57:31 +1100

    input field added with css styled

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import InputField from './Components/InputField';
 
 // a generic interface for the functional component type.
 // one of the two ways to write components in React.
@@ -7,6 +8,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <span className="heading">Taskify</span>
+      <InputField/>
     </div>
   );
 }

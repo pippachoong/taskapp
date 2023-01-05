d903af073ca7e2788412536fab2f84b1de07578a
Philippa Choong <pippachoong@gmail.com>
2023-01-05 23:53:27 +1100

    todo list component updated

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,20 +1,37 @@
 import React, { useState, useSyncExternalStore } from 'react';
 import './App.css';
 import InputField from './Components/InputField';
+import TodoList from './Components/TodoList';
+import {Todo} from "./model"
 
 // a generic interface for the functional component type.
 // one of the two ways to write components in React.
 const App: React.FC = () => {
 
   const [todo,setTodo] = useState<string>("")
+  //                                      ^ initial value of empty
   //                                ^ type of useState is defined. it can have union like < string | number >
+  const [todos,setTodos] = useState<Todo[]>([])
+  //                                  ^ import the model that created that has an interface defined for Todo.
+  //                                  ^ <Todo[]> this is how create an array of a type/interface
   console.log(todo);
   
+  const handleAdd = (e: React.FormEvent) => {
+    //                  ^ need to specify type for event variable in this function 
+    e.preventDefault();
+
+    if (todo){
+      setTodos([...todos,{ id: Date.now(), todo, isDone:false}])
+      setTodo("")
+    }
+
+  }
 
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField todo={todo} setTodo={setTodo}/>
+      <InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd}/>
+      <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
--- my-app/src/Components/InputField.tsx
+++ my-app/src/Components/InputField.tsx
@@ -1,18 +1,26 @@
-import React from 'react'
+import React, {useRef} from 'react'
 import "./styles.css"
 
 interface Props{
     todo: string;
     setTodo: React.Dispatch<React.SetStateAction<string>>;
     //          ^ this type is copied from App.tsx when setToDo variable is defined
+    handleAdd: (e: React.FormEvent) => void // void means function returns nothing 
 }
 
-const InputField = ({todo,setTodo}: Props) => {
+const InputField = ({todo,setTodo,handleAdd}: Props) => {
 //                                  ^ using interface to define the variable/argument types
+    const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input">
+    <form className="input" onSubmit = {(e)=> {
+        handleAdd(e)
+        inputRef.current?.blur()
+        }}>
         
-        <input type="input"
+        <input 
+            ref= {inputRef}
+            type="input"
             value={todo}
             onChange={(e)=> setTodo(e.target.value)} 
             placeholder="Enter a task"

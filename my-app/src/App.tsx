f3d266787482a57145ffaadb3df0725a29a20b24
Philippa Choong <pippachoong@gmail.com>
2023-01-05 22:18:49 +1100

    todo interface updated

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState, useSyncExternalStore } from 'react';
 import './App.css';
 import InputField from './Components/InputField';
 
 // a generic interface for the functional component type.
 // one of the two ways to write components in React.
 const App: React.FC = () => {
+
+  const [todo,setTodo] = useState<string>("")
+  //                                ^ type of useState is defined. it can have union like < string | number >
+  console.log(todo);
+  
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField/>
+      <InputField todo={todo} setTodo={setTodo}/>
     </div>
   );
 }
--- my-app/src/Components/InputField.tsx
+++ my-app/src/Components/InputField.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import "./styles.css"
 
-const InputField = () => {
+interface Props{
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    //          ^ this type is copied from App.tsx when setToDo variable is defined
+}
+
+const InputField = ({todo,setTodo}: Props) => {
+//                                  ^ using interface to define the variable/argument types
   return (
     <form className="input">
         
-        <input type="input" placeholder="Enter a task" className="input_box"/>
+        <input type="input"
+            value={todo}
+            onChange={(e)=> setTodo(e.target.value)} 
+            placeholder="Enter a task"
+            className="input_box"
+        />
         <button className="input_submit" type="submit">
             Go
         </button>

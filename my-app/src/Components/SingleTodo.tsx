b37120c81519e358f59e80687af68b64f3d2b2bf
Philippa Choong <pippachoong@gmail.com>
2023-01-06 11:17:54 +1100

    initialised done function

--- my-app/src/Components/SingleTodo.tsx
+++ my-app/src/Components/SingleTodo.tsx
@@ -3,6 +3,7 @@ import { Todo } from "../model"
 import {AiFillEdit,AiFillDelete} from "react-icons/ai"
 import {MdDone} from "react-icons/md"
 import "./styles.css"
+import TodoList from './TodoList'
 
 
 type Props = {
@@ -12,9 +13,26 @@ type Props = {
 }
 
 const SingleTodo = ({todo,todos,setTodos}:Props) => {
+
+    const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+
   return (
    <form className="todos_single">
-    <span className="todos_single--text">{todo.todo}</span>
+    {
+        todo.isDone? (
+            <s className="todos_single--text">{todo.todo}</s>
+
+        ):(
+              <span className="todos_single--text">{todo.todo}</span>
+        )
+    }
         <div>
             <span className="icon">
                 <AiFillEdit/>
@@ -22,7 +40,7 @@ const SingleTodo = ({todo,todos,setTodos}:Props) => {
             <span className="icon">
                 <AiFillDelete/>
             </span>
-            <span className="icon">
+            <span className="icon" onClick={()=>handleDone(todo.id)}>
                 <MdDone/>    
             </span>
         </div>

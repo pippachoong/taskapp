60eadc487f38d4dffc08c57e956f7ce116e7a5a1
Philippa Choong <pippachoong@gmail.com>
2023-01-06 11:09:01 +1100

    update single todo styling

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # taskapp
+## React & typescript
--- my-app/src/Components/SingleTodo.tsx
+++ my-app/src/Components/SingleTodo.tsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Todo } from "../model"
+import {AiFillEdit,AiFillDelete} from "react-icons/ai"
+import {MdDone} from "react-icons/md"
+import "./styles.css"
 
-const SingleTodo = () => {
+
+type Props = {
+    todo: Todo;
+    todos: Todo[];
+    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+}
+
+const SingleTodo = ({todo,todos,setTodos}:Props) => {
   return (
-    div.
+   <form className="todos_single">
+    <span className="todos_single--text">{todo.todo}</span>
+        <div>
+            <span className="icon">
+                <AiFillEdit/>
+            </span>
+            <span className="icon">
+                <AiFillDelete/>
+            </span>
+            <span className="icon">
+                <MdDone/>    
+            </span>
+        </div>
+   </form>
   )
 }
 
